1888cc6ab6355ebb501fd5b90f11ba4aed7bf196
Demam20 <59580953+Demam20@users.noreply.github.com>
2020-03-20 19:53:52 -0700

    register/login routs/validaion

--- routes/api/users.js
+++ routes/api/users.js
@@ -2,12 +2,21 @@ const express = require('express');
 const User = require('../../models/User');
 const gravatar = require('gravatar');
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const keys = require('../../config/keys');
+const passport = require('passport');
 const router = express.Router();
+const validateRegisterInput = require('../../validation/register');
+const validateLoginInput = require('../../validation/login');
 
 // @route   POST api/users/register
 // @desc    Register user
 // @access  Public
 router.post('/register', (req, res) => {
+   const {errors, isValid} = validateRegisterInput(req.body)
+  if(!isValid) {
+    return res.status(400).json(errors);
+  }
   User.findOne({email: req.body.email})
    .then(user => {
     if (user){
@@ -40,7 +49,73 @@ router.post('/register', (req, res) => {
 
    )
    .catch(err => console.log(err))
-  })
+  });
+
+// @route   POST api/users/login
+// @desc    Login user
+// @access  Public
+router.post('/login', (req,res)=> {
+  const {errors, isValid} = validateLoginInput(req.body)
+  if(!isValid) {
+    return res.status(400).json(errors);
+  }
+  
+  User.findOne({email: req.body.email})
+.then(user => {
+  if (!user){
+    return res.status(404).json({email: 'User not found!'});
+  } else {
+    bcrypt.compare(req.body.password, user.password)
+      .then(isMatch => {
+        if (isMatch){
+          //User matched
+          //payload
+              const payload = {
+                id: user.id, 
+                name: user.name, 
+                avatar: user.avatar};
+              
+                
+          //sign token
+                jwt.sign(
+                  payload, 
+                  keys.secretOrKey, 
+                  {expiresIn: 3600}, 
+                  (err, token) => {
+                    res.json({
+                      success: true,
+                      token: 'Bearer ' + token
+                    })
+                  } )
+          
+        } else {
+          return res.status(400).json({password:'password incorrect!'});
+        }  
+      })
+
+  }
+
+})
+.catch(err => console.log(err));
+})
+
+// @route   GET api/users/current
+// @desc    Return current user information
+// @access  Private
+router.get(
+  '/current',
+  passport.authenticate('jwt', {session: false}),
+  (req, res) => {
+    res.json({ 
+      id: req.user.id,
+      name: req.user.name,
+      email: req.user.email
+    });
+  }
+);
+
+
+
 
 
 

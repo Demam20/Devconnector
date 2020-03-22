9f17a8545f71d2c9e447e7f27ab8a34e26f3e202
Demam20 <59580953+Demam20@users.noreply.github.com>
2020-03-21 19:13:58 -0700

    adding keys

---
+++ config/keys.js
@@ -0,0 +1,8 @@
+module.exports = {
+  mongoURI :
+    `mongodb+srv://Team4:${encodeURIComponent(2020@Feb29)}@snapshare-fv8zk.mongodb.net/test?retryWrites=true&w=majority`,
+    
+    
+    secretOrKey: 'secret'
+
+};

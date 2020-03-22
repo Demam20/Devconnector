5b39839a9b590ea91febb5f5ec9958f1e1ff6026
Demam20 <59580953+Demam20@users.noreply.github.com>
2020-03-21 20:09:01 -0700

    fixed key

--- config/keys.js
+++ config/keys.js
@@ -1,6 +1,6 @@
 module.exports = {
   mongoURI :
-    `mongodb+srv://Team4:${encodeURIComponent(2020@Feb29)}@snapshare-fv8zk.mongodb.net/socialdb?retryWrites=true&w=majority`,
+    `mongodb+srv://Team4:${encodeURIComponent("2020@Feb29")}@snapshare-fv8zk.mongodb.net/socialdb?retryWrites=true&w=majority`,
     
     
     secretOrKey: 'secret'

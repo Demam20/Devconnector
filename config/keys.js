718e60693e2be118f7c99caa42f40dc2694b6ecc
Demam20 <59580953+Demam20@users.noreply.github.com>
2020-03-21 21:43:02 -0700

    keys deleting

--- config/keys.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  mongoURI :
-    `mongodb+srv://Team4:${encodeURIComponent("2020@Feb29")}@snapshare-fv8zk.mongodb.net/socialdb?retryWrites=true&w=majority`,
-    
-    
-    secretOrKey: 'secret'
-
-};

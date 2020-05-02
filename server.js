70bb1bedff2a5dfce28a1b3fca55c406a0a20873
Vaishnavi-CR <59356973+Vaishnavi-CR@users.noreply.github.com>
2020-05-02 00:47:55 -0700

    prepared to deploy

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const profile = require('./routes/api/profile');
 const posts = require('./routes/api/posts');
 const passport = require('passport'); 
 const app = express();
+const path = require('path')
 
 //Body parser middleware
 app.use(bodyParser.urlencoded({extended: false}));
@@ -25,14 +26,17 @@ app.use(passport.initialize());
 //Passport config
 require('./config/passport')(passport)
 
-
-// let's write our first route
-app.get('/',(req,res) => res.send('hello Doaa')
-
-);
 app.use('/api/users', users);
 app.use('/api/profile', profile);
 app.use('/api/posts', posts);
 
-const port = 8030;
+if(process.env.NODE_ENV === 'production') {
+  //set static folder nad start index.html
+  app.use(express.static('client/build'))
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+  })
+}
+
+const port = process.env.PORT || 8030;
 app.listen(port, () => console.log(`Server running on port ${port}`));
